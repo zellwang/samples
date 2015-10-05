a49f40de7e30c4d74fad6740ceac37c3557c2826
zellwang <zellwang@126.com>
2015-10-05 22:04:58 +0800

    add variable and unused function

--- require/js/lib/math.js
+++ require/js/lib/math.js
@@ -1,12 +1,18 @@
 define(function (){
+    var magicNumber = 42;
     var add = function (x,y){     //每个函数都以变量的形式定义
         return x+y;
     };
     var minus = function (x,y){
         return x-y;
     };
+    var useless = function (x,y){
+        return 0;
+    };
     return {
-        add: add,       // 第一个add表示对外暴露的方法名，第二个add表示该方法对应于第二行定义的add方法
-        minus: minus
+        add: add,                 // 第一个add表示对外暴露的方法名，第二个add表示该方法对应于第二行定义的add方法
+        minus: minus,
+        magicNumber: magicNumber  // 暴露变量
+                                  // 不暴露useless方法
     };
 });
--- require/js/main.js
+++ require/js/main.js
@@ -7,6 +7,9 @@ require.config({
 
 require(['jquery', 'math'],                 // 依赖的js文件
     function ($, math){                     // 这两个js文件将以对象的形式被使用，对象名分别是$和math
-        $("div").html(math.add(1,1));       // math.js中定义的add方法
+        $("div").html("magic number:" 
+                    + math.magicNumber      // math.js中定义的magicNumber变量
+                    + ";  1+1=" 
+                    + math.add(1,1));       // math.js中定义的add方法
     }
 );
